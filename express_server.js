05d7db1deca93210e06b04fcff72737918138167
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-18 18:14:52 -0500

    Added route handler for /urls/new

--- express_server.js
+++ express_server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,10 +31,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index",templateVars)
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL , longURL:  urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
