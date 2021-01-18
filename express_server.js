cd7b3db1aebf3410a5e9ddd3a7703838513fcda6
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-18 17:00:27 -0500

    Added handler for short url route

--- express_server.js
+++ express_server.js
@@ -23,7 +23,12 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render("urls_index",templateVars)
+})
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL , longURL:  urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 })
 
 app.listen(PORT, () => {
