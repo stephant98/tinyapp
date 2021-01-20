93561db820f55a5aba0b397f341a651d460448db
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-19 20:11:55 -0500

    Added POST route to clear cookies when logout

--- express_server.js
+++ express_server.js
@@ -34,6 +34,11 @@ app.post("/login", (req, res) => {
   res.redirect('/urls')
 })
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls')
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
