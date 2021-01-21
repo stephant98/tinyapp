a9020565530c4e0f5f31925a6e19540da3891d1e
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-21 18:51:59 -0500

    Created file with helper functions

---
+++ helpers.js
@@ -0,0 +1,35 @@
+function generateRandomString() {
+  const randomNumber = Math.random().toString(36).substring(7);
+  return randomNumber;
+};
+
+
+const getUserByEmail = function(email, database) {
+  let user;
+  for(const key in database) {
+    if(database[key].email === email){
+      user = database[key]
+    }
+  }
+  return user
+};
+
+
+const urlsForUser = function(id) {
+  let newObj = {};
+  
+  for (const key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      newObj[key] = urlDatabase[key].longURL 
+    }
+  }
+  return newObj;
+}
+
+
+module.exports = { generateRandomString, getUserByEmail, urlsForUser }
+
+
+
+
+
